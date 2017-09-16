b45a0d55da0dc82e5c49ad61891bf773d862a534
Liam Kelly <liamptk@gmail.com>
2017-09-16 09:23:10 +0100

    colour changes

--- installation.php
+++ installation.php
@@ -8,10 +8,8 @@ include_once('header.php');
 
             <div class="col-lg-12">
                 <h1 class="page-header" style="color:#009ce6;">Installation...</h1>
-				<p>At Lumelec we offer a second to none electrical installation service. As a STROMA certified approved contractor & Part-P registered we offer a wealth of experience across all areas of the electrical sector from domestic & commercial right through to industrial installations.</p>
+				<p class="services">At Lumelec we offer a second to none electrical installation service. As a STROMA certified approved contractor & Part-P registered we offer a wealth of experience across all areas of the electrical sector from domestic & commercial right through to industrial installations.</p>
             </div>
-
-
         </div>
         <!-- /.row -->
 
